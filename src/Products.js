198c6e6ea6cde43c7f3275451d8a54724934de5f
Sushant-Luitel <sushantluitel39@gmail.com>
2024-01-20 09:33:25 +0545

    image in src folder

--- src/Products.js
+++ src/Products.js
@@ -1,67 +1,63 @@
+import img1 from "./assets/1.jpg";
+import img2 from "./assets/2.jpg";
+import img3 from "./assets/3.jpg";
+import img4 from "./assets/4.jpg";
+import img5 from "./assets/5.jpg";
+import img6 from "./assets/6.jpg";
+import img7 from "./assets/7.jpg";
+import img8 from "./assets/8.jpg";
+
 export const Products = [
   {
     id: 1,
     name: "Hakuna Matata",
-    image:
-      "https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/392743165_360206886346163_2655292747188386134_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lBSdblaTaLkAX8hbjO-&_nc_ht=scontent.fktm20-1.fna&oh=00_AfDp42h9LUlY8AhZTnnIJmP9kaDfQDFWBykQRxYYY6pJ5w&oe=6541DAF0",
+    image: img1,
     price: 1200.0,
   },
   {
     id: 2,
     name: "Buddha",
-    image:
-      "https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/274960194_241549624859288_6835918109597001752_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=qmsNFCNOQk8AX8_uld4&_nc_ht=scontent.fktm20-1.fna&oh=00_AfB1G-afe2S5fiTxax2YpWWEBBZ8IAgMOaEFReDFz9ZGxA&oe=65427035",
+    image: img2,
     price: 1000.0,
   },
   {
     id: 3,
     name: "Wedding",
-    image:
-      "https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/356913872_303913278642191_4574109126270522654_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=VKfo_MUBC38AX9BaZ07&_nc_ht=scontent.fktm20-1.fna&oh=00_AfAquHVwjBVlOZamvmPxlbo-bx7DRfHzO6XnJR_GP7geIQ&oe=6541D062",
+    image: img3,
     price: 1500.0,
   },
   {
     id: 4,
     name: "Sisters",
-    image:
-      "https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/378312394_342921831408002_3085789799462884535_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=v9hxfiqvy5MAX_fddUM&_nc_ht=scontent.fktm20-1.fna&oh=00_AfAWsM3DR45saSTpr8JAKu2mLvHUY8Ar6G9R_RTcf9rLEA&oe=65414B9F",
+    image: img4,
     price: 1200.0,
   },
   {
     id: 5,
     name: "RCB Logo",
-    image:
-      "https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/358418235_308021848231334_1247393433416014048_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=3ATBIzcFIlMAX83u6Lc&_nc_ht=scontent.fktm20-1.fna&oh=00_AfCKa5fSLcvVCoilbkKwtnSjSGL-tttO9iEd1QeqY7_4oA&oe=654179A7",
+    image: img5,
     price: 1500.0,
   },
   {
     id: 6,
     name: "Connection",
-    image:
-      "https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/274922490_241550398192544_6499093472886159522_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=x7h4GQUZEVkAX-yte3O&_nc_ht=scontent.fktm20-1.fna&oh=00_AfA77_rX8qd4-uLhucm3fgFSLzU1QsR_8rs4kasePaf4Vw&oe=6541DB91",
+    image: img6,
     price: 1450.0,
   },
   {
     id: 7,
     name: "The Kiss",
-    image:
-      "https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/368679115_330317276001791_4749079513903479414_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=pMGgDMOh6wMAX8_jhI7&_nc_ht=scontent.fktm20-1.fna&oh=00_AfCw-j1TSwLMjhmkb4IN0rIRG6g84BJM7ZGdgUul7iZ9LQ&oe=65413D58",
+    image: img7,
     price: 4000.0,
   },
   {
     id: 8,
-    image:
-      "https://i.pinimg.com/736x/4f/8a/f0/4f8af08fa512f29f15d6a08a90a10906.jpg",
+    image: img8,
     price: 500,
   },
+
   {
     id: 9,
-    image:
-      "https://5.imimg.com/data5/SELLER/Default/2020/12/KU/FP/OM/8011079/green-laser-light-multi5-500x500.jpg",
-    price: 999,
-  },
-  {
-    id: 10,
     image:
       "https://cdn.oneesports.gg/cdn-data/2023/07/Gaming_LenovoLegionPro7i_keyboard.jpg",
     price: 200000,
